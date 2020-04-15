09082f23c2fae3f6f94ab3d7e579297292168d0b
Jewell <jhuffman815@gmail.com>
2020-04-14 22:40:18 -0400

    Toggle isFavorite

--- src/Context.js
+++ src/Context.js
@@ -10,8 +10,21 @@ function ContextProvider(props) {
             .then(data => setAllPhotos(data))
     }, [])
 
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if (photo.id === id) {
+                return {
+                    ...photo,
+                    isFavorite: !photo.isFavorite
+                }
+            }
+            return photo
+        })
+        setAllPhotos(updatedArr)
+    }
+
     return (
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {props.children}
         </Context.Provider>
     )
