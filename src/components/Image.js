dca2a363c1b472e0872155feb01d9bfbb33c7996
Jewell <jhuffman815@gmail.com>
2020-04-17 00:41:56 -0400

    Display Favorites

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,25 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const {toggleFavorite} = useContext
+
+    function heartIcon() {
+        if (img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if (hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
    return (
     <div className={`${className} image-container`}
          onMouseEnter={() => setHovered(true)}
          onMouseLeave={() => setHovered(false)}>
         <img src={img.url} className="image-grid" alt=""/>
-        {heartIcon}
+        {heartIcon()}
         {cartIcon}
     </div>
    )
