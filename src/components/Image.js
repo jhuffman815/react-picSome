06a7a6fc5d36a662ebe07874e53e4774e92f7b24
Jewell <jhuffman815@gmail.com>
2020-04-17 01:43:50 -0400

    Image PropTypes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,10 @@
 import React, {useState, useContext} from "react"
+import {Context} from "../Context"
+import PropTypes from "prop-types"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext
+    const {toggleFavorite} = useContext(Context)
 
     function heartIcon() {
         if (img.isFavorite) {
@@ -16,13 +18,22 @@ function Image({className, img}) {
 
    return (
     <div className={`${className} image-container`}
-         onMouseEnter={() => setHovered(true)}
+         onMouseEnter={() => setHovered(true)} 
          onMouseLeave={() => setHovered(false)}>
         <img src={img.url} className="image-grid" alt=""/>
         {heartIcon()}
         {cartIcon}
     </div>
    )
+}
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
 }
 
 export default Image
